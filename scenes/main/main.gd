40850c94363d67c97dd606a946ac2b180279cbec
CJ DiMaggio <cjssawa@gmail.com>
2020-05-28 04:47:40 -0400

    Update

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,8 +1,16 @@
 extends Spatial
 
+var captured_mouse = false
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	captured_mouse = true
 
 func _input(event):
 	if event.is_action_pressed('ui_cancel'):
-		get_tree().quit()
+		if self.captured_mouse:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			captured_mouse = false
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			captured_mouse = true
